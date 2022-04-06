82431ae5a2bf004c256b6635cee269af00938c3c
yosef <yoshissac6@gmail.com>
2022-04-06 12:54:38 +0800

    Fix: Use point at the end of messages instead of `(buffer-size)'
    to check if there're new messages in the irc buffer.

--- erc-logger.el
+++ erc-logger.el
@@ -5,6 +5,9 @@
 ;;  \___|_|  \___|    |_|\___/ \__, |\__, |\___|_|   
 ;;                             |___/ |___/           
 
+(require 'erc)
+(require 'cl)
+
 (defvar *erc-logger-log-directory* nil)
 (defvar *erc-logger-log-other-directory* nil)
 (defvar *erc-logger-log-timer* nil)
@@ -19,6 +22,15 @@
       (insert-buffer cur-buffer)
       (write-file filename))))
 
+(defun erc-logger-end-of-messages ()
+  (save-excursion
+    (goto-char (point-max))
+    (previous-line)
+    (end-of-line)
+    (end-of-line)
+    (end-of-line)
+    (point)))
+
 (defun erc-logger-log-start ()
   (interactive)
   (setq *erc-logger-log-directory* (file-name-as-directory *erc-logger-log-directory*)
@@ -28,9 +40,10 @@
     (with-current-buffer (current-buffer)
       (switch-to-buffer erc-buffer)
       (unless (gethash erc-buffer *erc-logger-irc-buffer-size-map*)
-	(puthash erc-buffer (buffer-size) *erc-logger-irc-buffer-size-map*))))
+	(puthash erc-buffer (erc-logger-end-of-messages)
+		 *erc-logger-irc-buffer-size-map*))))
   (setq *erc-logger-log-todays-date* (datetime-format "%Y-%m-%d")
-	*erc-logger-log-timer* (run-at-time "1 sec" 10 #'erc-log-buffers)))
+	*erc-logger-log-timer* (run-at-time "1 sec" 6 #'erc-log-buffers)))
 
 (defun erc-logger-log-stop ()
   (interactive)
@@ -44,16 +57,14 @@
 	       (dolist (erc-buffer (erc-buffer-list))
 		 (switch-to-buffer erc-buffer)
 		 (let* ((file-name (concat (buffer-name erc-buffer)
-					   ;;(datetime-format "_%Y-%m-%d_%H:%M:%S.txt")
 					   (datetime-format "_%Y-%m-%d.txt")))
 			(file-full-path (concat *erc-logger-log-directory*
-						"/" file-name))
-			(buffer-size (buffer-size)))
+						"/" file-name)))
 		   (if (string= *erc-logger-log-todays-date* (datetime-format "%Y-%m-%d"))
 		       (progn
-			 (when (not (= buffer-size (gethash erc-buffer *erc-logger-irc-buffer-size-map*)))
+			 (when (not (= (erc-logger-end-of-messages) (gethash erc-buffer *erc-logger-irc-buffer-size-map*)))
 			   (write-file-immut file-full-path)
-			   (puthash erc-buffer buffer-size *erc-logger-irc-buffer-size-map*)))
+			   (puthash erc-buffer (erc-logger-end-of-messages) *erc-logger-irc-buffer-size-map*)))
 
 		     ;; compress log files and mv them to another directory on next day
 		     ;; and clear the buffer, save to new files
@@ -65,7 +76,7 @@
 							   (rename-file file-full-path dir-name t)
 							   (unless
 							       (= 0 (shell-command
-								     (concat "gzip -9 -f "
+								     (concat "gzip -9f "
 									     dir-name
 									     file-name)))
 							     (message "failed to compress file!"))))
@@ -79,3 +90,5 @@
 	     (unless (string= *erc-logger-log-todays-date* (datetime-format "%Y-%m-%d"))
 	       (setq *erc-logger-log-todays-date* (datetime-format "%Y-%m-%d"))))
     (error "Invalid directory name, please set variable `*erc-logger-log-directory*' properly.")))
+
+(provide 'erc-logger)
